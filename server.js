0c074e5319af2d581070aea67f6b0e52dfe44690
MotilalChavhan <motilalvijaychavhan@gmail.com>
2019-08-11 11:50:07 +0530

    changing bcrypt to bcryptjs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 const saltRounds = 10;
